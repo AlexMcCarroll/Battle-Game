48ef2714b701cee22ff85f8380286f78f24fecb2
AlexMcCarroll <alexandra@mccarrollhk.com>
2018-03-09 10:59:34 +0000

    glabal variables added

--- app.rb
+++ app.rb
@@ -9,38 +9,38 @@ class Battle < Sinatra::Base
   end
 
   post '/name' do
-    session[:player1] = params[:player1]
-    session[:player2] = params[:player2]
+    $player1 = Player.new(params[:player1])
+    $player2 = Player.new(params[:player2])
     redirect to('/play')
   end
 
   get '/play' do
-    @player1 = session[:player1]
-    @player2 = session[:player2]
+    @player1 = $player1.name
+    @player2 = $player2.name
     erb(:play)
   end
 
   post '/attack_1' do
-    @player1 = session[:player1]
-    @player2 = session[:player2]
+    @player1 = $player1.name
+    @player2 = $player2.name
     redirect to('/confirmation1')
   end
 
   post '/attack_2' do
-    @player1 = session[:player1]
-    @player2 = session[:player2]
+    @player1 = $player1.name
+    @player2 = $player2.name
     redirect to('/confirmation2')
   end
 
   get '/confirmation1' do
-    @player1 = session[:player1]
-    @player2 = session[:player2]
+    @player1 = $player1.name
+    @player2 = $player2.name
     erb(:confirmation1)
   end
 
   get '/confirmation2' do
-    @player1 = session[:player1]
-    @player2 = session[:player2]
+    @player1 = $player1.name
+    @player2 = $player2.name
     erb(:confirmation2)
   end
 
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,6 +4,8 @@ describe Player do
   subject(:alex) { Player.new('Alex') }
 
   describe '#name' do
-    
+    it 'returns the name' do
+      expect(alex.name).to eq 'Alex'
+    end
   end
 end
